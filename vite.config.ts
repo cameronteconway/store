38e171090f62718050217f355cd344e62595021d
cameronteconway <cameronteconway@outlook.com>
2023-07-16 14:29:49 +0100

    service worker

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                app: './index.html',
+                'service-worker': './service-worker.js',
+            },
+            output: {
+                entryFileNames: (assetInfo) => {
+                    return assetInfo.name === 'service-worker'
+                        ? '[name].js' // put service worker in root
+                        : 'assets/js/[name]-[hash].js'; // others in `assets/js/`
+                },
+            },
+        },
+    },
 });
