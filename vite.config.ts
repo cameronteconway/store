035665d0a7e10c04bc45fdd504af8d50952da957
cameronteconway <cameronteconway@outlook.com>
2023-07-16 14:32:52 +0100

    Typescript service worker

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 app: './index.html',
-                'service-worker': './service-worker.js',
+                'service-worker': './service-worker.ts',
             },
             output: {
                 entryFileNames: (assetInfo) => {
